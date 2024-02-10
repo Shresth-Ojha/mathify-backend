5d11a49fdcf827c1cd0f40675b8635453045438d
Shresth-Ojha <shresthojha.iitdelhi@gmail.com>
2024-02-11 00:11:20 +0530

    added express-async-handler feature -> removed trycatch

--- src/controllers/report.ts
+++ src/controllers/report.ts
@@ -1,55 +1,55 @@
 import { RequestHandler } from 'express';
+import asyncHandler from 'express-async-handler';
+
 import Report from '../models/report';
 import ProjectError from '../helper/error';
 
 import { ResultResponse } from '../utils/interfaces';
 
-const getReport: RequestHandler = async (req, res, next) => {
-    try {
-        let report;
-        if (req.params.reportId) { // single report
-            const reportId = req.params.reportId;
-            const singleReport = await Report.findById(reportId);
+const getReport: RequestHandler = asyncHandler(async (req, res, next) => {
+    let report;
+    if (req.params.reportId) {
+        // single report
+        const reportId = req.params.reportId;
+        const singleReport = await Report.findById(reportId);
 
-            if (!singleReport) {
-                const err = new ProjectError("Report does'nt exist.");
-                err.statusCode = 404;
-                throw err;
-            }
-
-            const userId = req.userId;
-            const reportOf = singleReport.userId;
-
-            if (userId !== reportOf.toString()) {
-                const err = new ProjectError(
-                    `You can't access other user's report.`
-                );
-                err.statusCode = 405;
-                throw err;
-            }
-            report = [singleReport];
-        } else { // all reports
-            const userId = req.userId;
-            report = await Report.find({ userId });
-
-            if (!report) {
-                const err = new ProjectError("Report does'nt exist.");
-                err.statusCode = 404;
-                throw err;
-            }
+        if (!singleReport) {
+            const err = new ProjectError("Report does'nt exist.");
+            err.statusCode = 404;
+            throw err;
         }
 
-        const resp: ResultResponse = {
-            status: 'success',
-            message: req.params.reportId
-                ? `Fetched the report`
-                : `Fetched the reports`,
-            data: { report: report },
-        };
-        res.send(resp);
-    } catch (error) {
-        next(error);
+        const userId = req.userId;
+        const reportOf = singleReport.userId;
+
+        if (userId !== reportOf.toString()) {
+            const err = new ProjectError(
+                `You can't access other user's report.`
+            );
+            err.statusCode = 405;
+            throw err;
+        }
+        report = [singleReport];
+    } else {
+        // all reports
+        const userId = req.userId;
+        report = await Report.find({ userId });
+
+        if (!report) {
+            const err = new ProjectError("Report does'nt exist.");
+            err.statusCode = 404;
+            throw err;
+        }
     }
-};
+
+    const resp: ResultResponse = {
+        status: 'success',
+        message: req.params.reportId
+            ? `Fetched the report`
+            : `Fetched the reports`,
+        data: { report: report },
+    };
+    res.send(resp);
+});
 
 export { getReport };
--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -1,96 +1,82 @@
 import { RequestHandler } from 'express';
+import asyncHandler from 'express-async-handler';
 
 import User from '../models/user';
 import ProjectError from '../helper/error';
 
 import { ResultResponse } from '../utils/interfaces';
 
-const getUser: RequestHandler = async (req, res, next) => {
+const getUser: RequestHandler = asyncHandler(async (req, res, next) => {
     let resp: ResultResponse;
     // console.log('assed the authentication middleware');
 
-    try {
-        let userId = req.body.userId;
+    let userId = req.body.userId;
 
-        if(userId){
-
-            if (req.userId != userId) {
-                const err = new ProjectError('Please apni hi ID maange');
-                err.statusCode = 401;
-                throw err;
-            }
-        }
-        else {
-            userId = req.userId.toString();
-        }
-
-
-        const user = await User.findById(userId, { name: 1, email: 1 });
-
-        if (!user) {
-            const err = new ProjectError(`User with id ${userId} not found`);
-            err.statusCode = 401;
-            throw err;
-        } else {
-            resp = {
-                status: 'success',
-                message: `User Found!`,
-                data: { user },
-            };
-            res.status(200).send(resp);
-        }
-    } catch (error) {
-        next(error);
-    }
-};
-
-const updateUser: RequestHandler = async (req, res, next) => {
-    let resp: ResultResponse;
-
-    try {
-        const userId = req.body._id;
-
+    if (userId) {
         if (req.userId != userId) {
-            const err = new ProjectError('Please apni hi ID ko update kare');
+            const err = new ProjectError('Please apni hi ID maange');
             err.statusCode = 401;
             throw err;
         }
-
-        const user = await User.findById(userId);
-        if (!user) {
-            const err = new ProjectError(`User not found`);
-            err.statusCode = 401;
-            throw err;
-        } else {
-            user.name = req.body.name;
-            await user.save();
-            resp = {
-                status: 'success',
-                message: `User updated`,
-                data: {},
-            };
-            res.status(200).send(resp);
-        }
-    } catch (error) {
-        next(error);
+    } else {
+        userId = req.userId.toString();
     }
-};
 
-const logoutUser:RequestHandler = async (req,res,next) => {
-    let resp: ResultResponse;
+    const user = await User.findById(userId, { name: 1, email: 1 });
 
-    try {
-        res.clearCookie('jwttoken');
-        console.log('Logged Out')
+    if (!user) {
+        const err = new ProjectError(`User with id ${userId} not found`);
+        err.statusCode = 401;
+        throw err;
+    } else {
         resp = {
             status: 'success',
-            message: `User logged out`,
+            message: `User Found!`,
+            data: { user },
+        };
+        res.status(200).send(resp);
+    }
+});
+
+const updateUser: RequestHandler = asyncHandler(async (req, res, next) => {
+    let resp: ResultResponse;
+
+    const userId = req.body._id;
+
+    if (req.userId != userId) {
+        const err = new ProjectError('Please apni hi ID ko update kare');
+        err.statusCode = 401;
+        throw err;
+    }
+
+    const user = await User.findById(userId);
+    if (!user) {
+        const err = new ProjectError(`User not found`);
+        err.statusCode = 401;
+        throw err;
+    } else {
+        user.name = req.body.name;
+        await user.save();
+        resp = {
+            status: 'success',
+            message: `User updated`,
             data: {},
         };
-        res.status(200).send(resp)
-    } catch (error) {
-        next(error)
+        res.status(200).send(resp);
     }
-}
+});
+
+const logoutUser: RequestHandler = asyncHandler(async (req, res, next) => {
+    let resp: ResultResponse;
+
+    res.clearCookie('jwttoken');
+    console.log('Logged Out');
+    resp = {
+        status: 'success',
+        message: `User logged out`,
+        data: {},
+    };
+    res.status(200).send(resp);
+});
 
 export { getUser, updateUser, logoutUser };
